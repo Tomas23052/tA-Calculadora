dbc4e2e60e15c53b1b2add47dc3d457415d2e008
Tomás Silva <aluno23052@ipt.pt>
2022-03-22 14:48:11 +0000

    adição do processamento dos algarismos

--- tA--Calculadora/tA--Calculadora/Controllers/HomeController.cs
+++ tA--Calculadora/tA--Calculadora/Controllers/HomeController.cs
@@ -16,12 +16,71 @@ namespace tA__Calculadora.Controllers
         [HttpGet] // este anotador é facultativo
         public IActionResult Index()
         {
+            //preparar os dados a serem enviados para a View na primeira interação
+
+            ViewBag.Visor = "0";   
             return View();
         }
 
         [HttpPost] // só quando o formulário for submetido em 'post' ele será acionado
-        public IActionResult Index(string botao)
+        public IActionResult Index(string botao, string visor)
         {
+
+            switch (botao)
+            {
+                case "1":
+                case "2":
+                case "3":
+                case "4":
+                case "5":
+                case "6":
+                case "7":
+                case "8":
+                case "9":
+                case "0":
+                    //pressionei um algarismo
+                    if(visor == "0")
+                    {
+                        visor = botao;
+                    }
+                    else { visor += botao; }
+                    //desafio : fazer em modo algébrico esta operação
+                    break;
+                case ",":
+                    //foi pressionado ','
+                    if (visor.Contains(",") == false)
+                    {
+                        visor += botao;
+                    }
+                    
+                    break;
+                case "C":
+                    //foi pressionado 'C'
+                    if(visor != "0")
+                    {
+                        visor = "0";
+                    }
+                    break;
+                case "+/-":
+                    //vamos inverter o valor do "visor"
+                    if (visor.StartsWith("-")){
+                        visor = visor.Substring(1);
+                    }
+                    else
+                    {
+                        visor = "-" + visor;
+                    }
+                    
+                    break;
+                
+
+            }
+
+            //preparar dados para serem enviados à View
+            ViewBag.Visor = visor;
+            
+            
+
             return View();
         }
 
